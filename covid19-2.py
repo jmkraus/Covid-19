cb45c17e5da4fc078b5492a8a9f45f3f873680cb
Jürgen Kraus <jmkraus@users.noreply.github.com>
2020-04-17 18:57:21 +0200

    New REST endpoint

--- covid19-2.py
+++ covid19-2.py
@@ -24,7 +24,7 @@ def read_data(force=False):
 
 	if force is True or not os.path.isfile(
 			filename) or os.path.getmtime(filename) < time.time() - delay:
-		r = requests.get('https://corona.lmao.ninja/countries')
+		r = requests.get('https://corona.lmao.ninja/v2/countries')
 		info = r.json()
 		with open(filename, 'w') as fp:
 			json.dump(info, fp)
@@ -58,7 +58,7 @@ last_update = info[0]['updated']
 print('Last data update: ' + format_date(last_update))
 
 #configuration
-my_field = fields[1]
+my_field = fields[3]
 #world = True
 #debug_data(info, my_field)
 
@@ -78,14 +78,13 @@ countries = [x['country'] for x in my_info]
 data = [x[my_field] for x in my_info]
 
 y_pos = np.arange(len(countries))
-plt.rcdefaults()
 ax = plt.subplot(111)
-ax.barh(y_pos, data, align='center', alpha=0.5, color=my_colors)
+ax.barh(y_pos, data, height=0.8, align='center', alpha=0.5, color=my_colors)
 plt.yticks(y_pos, countries)
 plt.xlabel(my_field)
 plt.title('COVID-19')
 for i, v in enumerate(data):
-	ax.text(v, i - .15, ' ' + format_value(v), color='black', fontweight='normal')
+	ax.text(v, i - .15, ' ' + format_value(v), size='9', color='#555555', fontweight='normal')
 
 plt.show()
 
